dc6eaf88103fb1c2d343579b383ea51ea364c498
Priscila Leal <priscila-leal@outlook.com>
2023-06-16 20:55:11 -0300

    site trilha 2

--- codigo.js
+++ codigo.js
@@ -1,2 +1,12 @@
-//var descricao="BEM VINDO(A)!"
-//alert (descricao)
+var descricao="BEM VINDO(A)!"
+alert (descricao)
+
+
+
+var botão=document.getElementById('login-button');
+botão.addEventListener("click",clique,false)
+
+function clique(){
+    alert('Enviado!')
+}
+ 
